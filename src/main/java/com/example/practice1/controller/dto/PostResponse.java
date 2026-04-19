709e20bcae7d83bf6845bd88e2cdefcc2b155fc3
신다현 <shindahyun@sindahyeons-MacBook-Air.local>
2026-04-19 11:20:31 +0900

    fix: 정적 팩토리 메서드 활용

--- src/main/java/com/example/practice1/controller/dto/PostResponse.java
+++ src/main/java/com/example/practice1/controller/dto/PostResponse.java
@@ -1,27 +1,11 @@
 package com.example.practice1.controller.dto;
 
 import com.example.practice1.controller.entity.PostEntity;
-import lombok.Getter;
 
-@Getter
-public class PostResponse {
-    private final Long id;
-    private final String title;
-    private final String content;
-    private final String author;
+public record PostResponse(Long id, String title, String content, String author) {
 
-    public PostResponse(Long id, String title, String content, String author) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.author = author;
-    }
-
-    public PostResponse(PostEntity entity) {
-        this.id = entity.getId();
-        this.title = entity.getTitle();
-        this.content = entity.getContent();
-        this.author = entity.getAuthor();
-    }
-
+  public static PostResponse fromEntity(PostEntity postEntity) {
+    return new PostResponse(postEntity.getId(), postEntity.getTitle(), postEntity.getContent(),
+        postEntity.getAuthor());
+  }
 }
--- src/main/java/com/example/practice1/service/PostService.java
+++ src/main/java/com/example/practice1/service/PostService.java
@@ -4,56 +4,54 @@ import com.example.practice1.controller.dto.PostRequest;
 import com.example.practice1.controller.dto.PostResponse;
 import com.example.practice1.controller.entity.PostEntity;
 import com.example.practice1.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
-    private final PostRepository postRepository;
+  private final PostRepository postRepository;
 
-    public PostResponse save(PostRequest request) {
-        PostEntity entity = new PostEntity(request.title(), request.content(), request.author());
-        PostEntity returnedEntity = postRepository.save(entity);
+  public PostResponse save(PostRequest request) {
+    PostEntity entity = new PostEntity(request.title(), request.content(), request.author());
+    PostEntity returnedEntity = postRepository.save(entity);
 
-        return new PostResponse(returnedEntity.getId(), returnedEntity.getTitle(), returnedEntity.getContent(), returnedEntity.getAuthor());
+    return new PostResponse(returnedEntity.getId(), returnedEntity.getTitle(),
+        returnedEntity.getContent(), returnedEntity.getAuthor());
+  }
+
+  public List<PostResponse> findAll() {
+    List<PostEntity> entityList = postRepository.findAll();
+    List<PostResponse> responseList = new ArrayList<>();
+
+    for (PostEntity entity : entityList) {
+      PostResponse response = PostResponse.fromEntity(entity);
+      responseList.add(response);
     }
-
-    public List<PostResponse> findAll() {
-        List<PostEntity> entityList = postRepository.findAll();
-        List<PostResponse> responseList = new ArrayList<>();
-
-        for (PostEntity entity : entityList) {
-            PostResponse response = new PostResponse(entity.getId(),
-                    entity.getTitle(),
-                    entity.getContent(),
-                    entity.getAuthor());
-            responseList.add(response);
-        }
-        return responseList;
-    }
+    return responseList;
+  }
 
 
-    public PostResponse findById(Long id) {
-        PostEntity entity = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+  public PostResponse findById(Long id) {
+    PostEntity entity = postRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
-        return new PostResponse(entity.getId(), entity.getTitle(), entity.getContent(), entity.getAuthor());
-    }
+    return PostResponse.fromEntity(entity);
+  }
 
-    public PostResponse update(Long id, PostRequest postRequest) {
-        PostEntity entity = postRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-        // entity 수정로직
-        entity.update(postRequest.title(), postRequest.content(), postRequest.author());
-        return new PostResponse(entity);
-    }
+  public PostResponse update(Long id, PostRequest postRequest) {
+    PostEntity entity = postRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    // entity 수정로직
+    entity.update(postRequest.title(), postRequest.content(), postRequest.author());
+    return PostResponse.fromEntity(entity);
+  }
 
-    public void delete(Long id) {
-        postRepository.deleteById(id);
-    }
+  public void delete(Long id) {
+    postRepository.deleteById(id);
+  }
 }
 
